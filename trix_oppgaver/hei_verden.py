2ba83deaf97b3dd7055f8e32a4e3ada46757a745
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-18 11:37:14 +0200

    Delete other duplicate files

--- trix_oppgaver/hei_verden.py
+++
@@ -1,8 +0,0 @@
-alder = 4
-
-print("Hei Verden!")
-print("Alder: ", alder)
-
-#Nå ender vi verdien av variabelen alder
-alder = 32
-print("Ny alder: ", alder)
--- trix_oppgaver/person.py
+++
@@ -1,3 +0,0 @@
-print("John Doe")
-print("387-309-9999")
-print("123 Main Street, Anywhere, USA")
