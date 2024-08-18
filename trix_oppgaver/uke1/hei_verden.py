d0a444fe19a82e7048ebf2853610a1740139b494
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-18 11:29:27 +0200

    Create uke1 folder  and move files into it

---
+++ trix_oppgaver/uke1/hei_verden.py
@@ -0,0 +1,8 @@
+alder = 4
+
+print("Hei Verden!")
+print("Alder: ", alder)
+
+#Nå ender vi verdien av variabelen alder
+alder = 32
+print("Ny alder: ", alder)
---
+++ trix_oppgaver/uke1/person.py
@@ -0,0 +1,3 @@
+print("John Doe")
+print("387-309-9999")
+print("123 Main Street, Anywhere, USA")
