42ed5754bfa63bdd74d7baeb401e336367b42ff8
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-22 13:14:33 +0200

    Fixed if-statement logic

--- inleveringer/inlevering_1/meny.py
+++ inleveringer/inlevering_1/meny.py
@@ -6,7 +6,7 @@
 # reference them more than once
 hr1 = "Stor burger"
 hr2 = "BBQ"
-hr3 = "Vår fine salat"
+hr3 = "Salat"
 tb1 = "Pommes frites"
 tb2 = "Sennap"
 
@@ -29,12 +29,30 @@ print()
 #Ask the user to choose 1 main dish and 1 side dish
 print("Vennligst velg en hovedrett.")
 valg_hr = input()
+#if the input is not a valid main dish, end the program
+if (valg_hr != hr1) and (valg_hr != hr2) and (valg_hr != hr3):
+    print(f"{valg_hr} er ikke en gyldig hovedrett.")
+    exit("Vennligst prøv på nytt.")
+
 print("Vennligst velg ett tilbehør.")
 valg_tb = input()
+if valg_tb != tb1 and valg_tb != tb2:
+    print(f"{valg_tb} er ikke et gyldig tilbehør.")
+    exit("Vennligst prøv på nytt.")
 
 #Print a blank line for readability in the terminal
 print()
 
 #Check the user´s inputs and give a response
-if (valg_hr == hr1 or hr2) and (valg_tb == tb2):
-    print("Du spiser ikk nok grønnsaker!")
+
+#First, check if user chose no vegetables
+if (valg_hr == hr1 or valg_hr == hr2) and (valg_tb == tb2):
+    print("Du spiser ikke nok grønnsaker!")
+
+#Check if user chose a vegetarian meal
+elif valg_hr == hr3 and valg_tb == tb1:
+    print("Du har valgt et vagetarmåltid.")
+
+#Display what the user selected
+else:
+    print(f"Du har valgt {valg_hr} med {valg_tb}.")
