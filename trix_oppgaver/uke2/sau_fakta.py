4dfe320e60fd82702eff7763522d749c632a7920
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-23 15:03:06 +0200

    Create sau_fakta.py

---
+++ trix_oppgaver/uke2/sau_fakta.py
@@ -0,0 +1,10 @@
+def sheep_facts():
+    print("Sheep are silly critters.")
+    print("Sheep are soft!")
+    print("Sheep can lift 12 times their own weight!")
+    print("We get wool from sheep.")
+    print("Sheep are your friend! Bah!")
+
+sheep_facts()
+print("*** One more time! ***")
+sheep_facts()
