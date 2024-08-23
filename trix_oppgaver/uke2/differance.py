2501611ca16b8521cf5f75c6d0ff54d2fa2bf65b
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-23 13:41:48 +0200

    Create differance.py

---
+++ trix_oppgaver/uke2/differance.py
@@ -0,0 +1,7 @@
+def subtraction_time():
+    a = int(input("Write an integer: "))
+    b = int(input("That felt nice!  Do it again: "))
+    c = a-b
+    print(f"The differance between {a} and {b} is {c}")
+
+subtraction_time()
