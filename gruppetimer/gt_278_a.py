f1dea14fe7ea92f0cfa43a5f185ce5adf56e3ba8
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-27 11:24:40 +0200

    Create handletur.py and gruppetimer files

---
+++ gruppetimer/gt_278_a.py
@@ -0,0 +1,10 @@
+tekst = "hei", "verden"
+print(tekst) #prints out as:   ('hei', 'verden')
+
+print("hei", "verden2") #prints as:   hei verden2
+
+tekst2 = ("hei", "verden")
+print(tekst2)  #prints out as:   ('hei', 'verden')
+
+teks3 = "hei" + "verden"
+print(teks3) #prints out as:   heiverden
---
+++ gruppetimer/gt_278_b.py
@@ -0,0 +1,6 @@
+busselskap = "Unibuss"
+trikkeselskap = "Sporveien"
+if busselskap == trikkeselskap:
+    print("Samme selskap har både busser of trikker!")
+else:
+    print("Buss- og trikkeselskap er forskjellige!")
