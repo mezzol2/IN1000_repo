ac3e5a6c6f4f616ed981e80335801de2102718eb
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-27 15:46:15 +0200

    Finish handletur.py

--- trix_oppgaver/uke2/handletur.py
+++ trix_oppgaver/uke2/handletur.py
@@ -3,3 +3,18 @@ melk_pris = 15
 ost_pris = 40
 yoghurt_pris = 12
 
+print("Welcome to the store.")
+print("How many loafs of bread do you want?")
+brød_num = int(input())
+
+print("How many jugs of milk do you want?")
+melk_num = int(input())
+
+print("How many pieces of cheese, you fucking rat?")
+ost_num = int(input())
+
+print("How much yoghurt, you filthy animal?")
+yoghurt_num = int(input())
+
+total = brød_num*brød_pris + melk_num*melk_pris + ost_num*ost_pris + yoghurt_num*yoghurt_pris
+print(f"Pay up, bitch. You owe {total} kr.")
