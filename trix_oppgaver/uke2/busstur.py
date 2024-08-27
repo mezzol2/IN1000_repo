8e7d68eae87dea9f9f2eb529149ab9f933f32e50
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-27 16:51:10 +0200

    Create and start busstur.py

--- trix_oppgaver/uke2/busstur.py
+++ trix_oppgaver/uke2/busstur.py
@@ -5,6 +5,7 @@ boarding = " people board the buss"
 def walk():
     print(f"The buss is full! {extra} people must walk.")
 
+#station 1
 waiters = int(input(f"{station}1{message}\n"))
 if waiters <= 30 and waiters >=0:
     passengers = waiters
