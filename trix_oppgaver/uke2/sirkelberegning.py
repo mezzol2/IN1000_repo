0e4a74eda496e802231941483621db3e4e562432
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-27 16:25:49 +0200

    Add sirkelberegning.py

---
+++ trix_oppgaver/uke2/sirkelberegning.py
@@ -0,0 +1,9 @@
+radius = float(input("Hva er radiusen? "))
+pi = 3.141593
+diameter = 2 * radius
+omkrents = diameter * pi
+areal = pi * (radius**2)
+
+print(f'Diameter:{diameter:8.2f}')
+print(f'Areal:   {areal:8.2f}')
+print(f'Omkrets: {omkrents:8.2f}')
