25450a48ab2772db962175bdcd1abf5c201da58f
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-30 13:56:37 +0200

    Create gt_308.py

---
+++ gruppetimer/gt_308.py
@@ -0,0 +1,14 @@
+heltall1 = 1
+heltall2 = 2
+flyttall1 = 10.2
+flyttall2 = 83.29
+
+print(heltall1 + heltall2)
+print(heltall1*heltall2)
+print(flyttall1 + flyttall2)
+print(flyttall1*flyttall2)
+print(heltall2*flyttall2)
+print(heltall2*"hi")
+
+
+
