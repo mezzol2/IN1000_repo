db73569d62e8a145dd845c70546875ac3ea06b4e
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-30 13:09:25 +0200

    Add new lines to exit() phrases for readability

--- inleveringer/innlevering_2/wow_quiz.py
+++ inleveringer/innlevering_2/wow_quiz.py
@@ -5,7 +5,7 @@
 
 #Define a function that tells the user they entered an invalid value and end the program
 def error_mess():
-    print("It seems you are an agent of the Burning Legion and have given a corrupted response.")
+    print("\nIt seems you are an agent of the Burning Legion and have given a corrupted response.")
     exit("(Please try again with valid responses.)")
 
 
@@ -18,7 +18,7 @@ name = str(input("What is your name?\n"))
 #Determine if a melee or ranged play style is better.
 print(f"\n{name}!  That's a fine name indeed.  One that will be spoken of in future legends.")
 print("Now, to determeine which class you should play, I will ask a series of questions.")
-print(f"{name}, do you prefer to in the (1) thick of the action of to (2) fight at ranged?")
+print(f"{name}, do you prefer to in the (1) thick of the action, or to (2) fight at ranged?")
 melee_ranged = input("Repond by entering one of the numbered responses (1 or 2): ")
 
 #Support or damage
@@ -29,14 +29,14 @@ if dps_support == 2:
     heal_supp = int((input("\nDo you want to (1) heal your allies' wounds or (2) boost thier power? ")))
     #if the user wants to be support, but not heal, they should be an Augmenation Evoker
     if heal_supp == 2:
-        exit(f"{name}, you would be perfect as an Augmentation Evoker.  With your draconic powers,\nyou can help your allies defeat enemmies.")
+        exit(f"\n{name}, you would be perfect as an Augmentation Evoker.  With your draconic powers, you can help your allies defeat enemmies.")
     #If the user wants to be a melee healer, they coule be a Paladin or Monk
     elif melee_ranged == 1:
         heal_melee = int(input(f'{name}, do you (1) pray to the light or (2) meditate in your free time? '))
         if heal_melee == 1:
-            exit("You would be a great Holy Paladin.")
+            exit("\nYou would be a great Holy Paladin.")
         elif heal_melee == 2:
-            exit("Your dedication to meditation would make you a great Monk.")
+            exit("\nYour dedication to meditation would make you a great Monk.")
         else:
             error_mess()
     #if the user want to be a ranged healer, they can be a priest, shaman, druid, or evoker
@@ -44,12 +44,12 @@ if dps_support == 2:
     elif melee_ranged == 2:
         heal_ranged = int(input("Do you (1) praise the Light, (2) really like dragons, (3) a tree hugger, or (4) like totems?\n"))
         if heal_ranged ==1:
-            exit("Praise the Light! You have chosen the path of a Priest.")
+            exit("\nPraise the Light! You have chosen the path of a Priest.")
         elif heal_ranged == 2:
-            exit("See how scaley you are.  You shall be a Preservation Evoker.")
+            exit("\nSee how scaley you are.  You shall be a Preservation Evoker.")
         elif heal_ranged == 3:
-            exit("You shall be one with nature.  Embrace the path of a Druid.")
+            exit("\nYou shall be one with nature.  Embrace the path of a Druid.")
         elif heal_ranged == 4:
-            exit("Your will heal the world with your totems.  You are a Shaman of the elements.")
+            exit("\nßYour will heal the world with your totems.  You are a Shaman of the elements.")
         else:
             error_mess()
