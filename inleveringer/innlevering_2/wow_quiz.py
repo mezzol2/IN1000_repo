9c606f92ccd23e5ee512de26e20ffb2929072e39
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-08-30 13:21:20 +0200

    Fix logic when determining if the user should be a healer

--- inleveringer/innlevering_2/wow_quiz.py
+++ inleveringer/innlevering_2/wow_quiz.py
@@ -6,7 +6,7 @@
 #Define a function that tells the user they entered an invalid value and end the program
 def error_mess():
     print("\nIt seems you are an agent of the Burning Legion and have given a corrupted response.")
-    exit("(Please try again with valid responses.)")
+    exit("(Please try again with valid responses.)\n")
 
 
 #Greet the user and get their name
@@ -18,8 +18,8 @@ name = str(input("What is your name?\n"))
 #Determine if a melee or ranged play style is better.
 print(f"\n{name}!  That's a fine name indeed.  One that will be spoken of in future legends.")
 print("Now, to determeine which class you should play, I will ask a series of questions.")
-print(f"{name}, do you prefer to in the (1) thick of the action, or to (2) fight at ranged?")
-melee_ranged = input("Repond by entering one of the numbered responses (1 or 2): ")
+print(f"\n{name}, do you prefer to in the (1) thick of the action, or to (2) fight at ranged?")
+melee_ranged = int(input("Repond by entering one of the numbered responses (1 or 2): "))
 
 #Support or damage
 dps_support = int((input("\nDo want to (1) fight monsters directly or (2) aid your allies? ")))
@@ -29,27 +29,32 @@ if dps_support == 2:
     heal_supp = int((input("\nDo you want to (1) heal your allies' wounds or (2) boost thier power? ")))
     #if the user wants to be support, but not heal, they should be an Augmenation Evoker
     if heal_supp == 2:
-        exit(f"\n{name}, you would be perfect as an Augmentation Evoker.  With your draconic powers, you can help your allies defeat enemmies.")
-    #If the user wants to be a melee healer, they coule be a Paladin or Monk
-    elif melee_ranged == 1:
-        heal_melee = int(input(f'{name}, do you (1) pray to the light or (2) meditate in your free time? '))
-        if heal_melee == 1:
-            exit("\nYou would be a great Holy Paladin.")
-        elif heal_melee == 2:
-            exit("\nYour dedication to meditation would make you a great Monk.")
-        else:
-            error_mess()
-    #if the user want to be a ranged healer, they can be a priest, shaman, druid, or evoker
-    #ask the user for more information
-    elif melee_ranged == 2:
-        heal_ranged = int(input("Do you (1) praise the Light, (2) really like dragons, (3) a tree hugger, or (4) like totems?\n"))
-        if heal_ranged ==1:
-            exit("\nPraise the Light! You have chosen the path of a Priest.")
-        elif heal_ranged == 2:
-            exit("\nSee how scaley you are.  You shall be a Preservation Evoker.")
-        elif heal_ranged == 3:
-            exit("\nYou shall be one with nature.  Embrace the path of a Druid.")
-        elif heal_ranged == 4:
-            exit("\nßYour will heal the world with your totems.  You are a Shaman of the elements.")
-        else:
-            error_mess()
+        exit(f"\n{name}, you would be perfect as an Augmentation Evoker.  With your draconic powers, you can help your allies defeat enemmies.\n")
+    
+    #if the user wants to heal, figure determine what type of healer they should be
+    elif heal_supp ==1:
+        #If the user wants to be a melee healer, they coule be a Paladin or Monk
+        if melee_ranged == 1:
+            heal_melee = int(input(f'{name}, do you (1) pray to the light or (2) meditate in your free time? '))
+            if heal_melee == 1:
+                exit("\nYou would be a great Holy Paladin.\n")
+            elif heal_melee == 2:
+                exit("\nYour dedication to meditation would make you a great Monk.\n")
+            else:
+                error_mess()
+        #if the user want to be a ranged healer, they can be a priest, shaman, druid, or evoker
+        #ask the user for more information
+        elif melee_ranged == 2:
+            heal_ranged = int(input("Do you (1) praise the Light, (2) really like dragons, (3) a tree hugger, or (4) like totems?\n"))
+            if heal_ranged ==1:
+                exit("\nPraise the Light! You have chosen the path of a Priest.\n")
+            elif heal_ranged == 2:
+                exit("\nSee how scaley you are.  You shall be a Preservation Evoker.\n")
+            elif heal_ranged == 3:
+                exit("\nYou shall be one with nature.  Embrace the path of a Druid.\n")
+            elif heal_ranged == 4:
+                exit("\nßYour will heal the world with your totems.  You are a Shaman of the elements.\n")
+            else:
+                error_mess()
+    else:
+        error_mess()
