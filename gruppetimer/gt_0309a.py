55504c1349f3bf4ab1897085142da4ec4d181eef
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-03 11:30:45 +0200

    Remove case-sensitivity from gt_0309a.py

--- gruppetimer/gt_0309a.py
+++ gruppetimer/gt_0309a.py
@@ -2,7 +2,7 @@ amt = int(1000)
 print(f"Your account balance is {amt}.")
 action = input("Would you like to deposit or withdraw money?\nWrite d or w: ")
 
-if action == 'w':
+if action.lower() == 'w':
     change = int(input("How much would you like to withdraw: "))
     if change <= amt:
         amt = amt - change
@@ -11,7 +11,7 @@ if action == 'w':
         print(f'You are too broke to withdraw {change} as you only have {amt}.')
     else:
         print('You have entered an invalid input, and your account has been deleted.')    
-elif action == 'd':
+elif action.lower() == 'd':
     change = int(input("How much would you like to depost: "))
     amt = amt + change
     print(f'You have deposited {change}.  Your new balance is {amt}.')
