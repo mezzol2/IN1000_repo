292f72f3ea00a2d5e4951cef8a70d2c0c7e11cb8
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-04 11:03:14 +0200

    Create and finish matliste.py

---
+++ trix_oppgaver/uke3/matliste.py
@@ -0,0 +1,12 @@
+favorittmat = ['burger', 'pizza', 'sushi']
+print(favorittmat)
+
+favorittmat.pop(1)
+print(favorittmat)
+print(len(favorittmat))
+
+i = 0
+
+while i < 2:
+    print(favorittmat[i].upper())
+    i += 1
