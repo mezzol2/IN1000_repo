de64efe9b2e27f3e0b3d6be41edaf135fa9ca46a
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-05 20:35:33 +0200

    Add dictionary and if logic statements

--- inleveringer/innlevering_3/cat_translator.py
+++ inleveringer/innlevering_3/cat_translator.py
@@ -3,14 +3,48 @@
 #Welcome to the cat translator
 
 #Create a dictionary of cat noise
-cat_dict = {
-    'test' : 'very good'
+cat_words = {
+    'mew' : "Look at me, I'm a kitten.",
+    'meow' : "Notice me!",
+    'MEOWWW' : "Dinner is 30 seconds late, and I am dissatisfied, human.",
+    'purr' : "I am content with the current situation.",
+    'ekekekkekkek' : "Mother, I crave violence.",
+    'growl' : "Unhand me. I do not consent to this.",
+    '*stares silently*' : "The cat is attempting to communicate telepathically. Try listening."
 }
 
 #Define a function to return known cat "words"
 def show_words():
-    cat_keys = list(cat_dict.keys())
+    cat_keys = list(cat_words.keys())
     print("\nHere is a list of known cat words.")
     print(cat_keys)
+    print()
 
-show_words()
+#Greet the user
+print("\nHello, human.  Welcome to the cat translator.")
+#Ask the user if the need to see known cat words
+inp = input("Do you need to see the list of translatable cat words?\n[y/n] ")
+#Show the user the list if they want it
+if inp == 'y':
+    show_words()
+
+#Ask the user if they want to translate a word
+inp = input("Would you like to translate a cat word?\n[y/n?] ")
+#If the user wants a translation, ask them for a word
+if inp == 'y':
+    inp = input("\nWhat word do you need translated: ")
+    #If the user's word is in the cat_word dictionary, print the translation
+    if inp in cat_words:
+        print(f'\n{cat_words[inp]}\n')
+    
+    #If the user's word is not in cat_word, tell the user
+    else:
+        print("That word is not in the dictionary. Please try again.")
+
+#If the user does not want a translation, send them on their way    
+elif inp == 'n':
+    print("Guess you speak fluent cat already. BYE.\n")
+
+#If the user gave an invalid reponse, mock them
+else:
+    print("It's a yes or no question. Why are you making this hard?\n")
