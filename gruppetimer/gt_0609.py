c765bbbc18b11e24009eced17b8f49460899f4d5
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-06 13:28:11 +0200

    Create and edit gt_0609.py

---
+++ gruppetimer/gt_0609.py
@@ -0,0 +1,30 @@
+my_list = ['a', 'a','a' ]
+my_set = {1, 2, 3}
+my_dictionary = {
+    'word' : 'ord',
+    'text' : 'tekst',
+    'house' : 'hus'
+}
+
+list4 = "ldskfjhsdlh"
+print(len(list4))
+
+navn = ["Ola", "Martin", "Selma"]
+navn.append("Kari")
+print(navn)
+
+print(navn[1])
+
+navn.insert(0, "Austin")
+navn.insert(0, "Sally")
+print(navn)
+
+#nested lists
+koffert = [
+    ["tannbørste", "hårbørste", "tannkrem"],
+    ["jakke", "bukse", "sokker", "undertøy"],
+    ["kamera", "lommebok", "pass", "mobillader"]
+]
+
+bil = [koffert, "random shit"]
+print(bil)
