69cafbf37e26302a5f5cac70bf61de0e49d9be2a
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-10 10:58:15 +0200

    Create and finish løkker.py

---
+++ trix_oppgaver/uke4/løkker.py
@@ -0,0 +1,16 @@
+# for x in range(11):
+#     print(x)
+
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+liste = ["Sauer", "er", "myke", "dyr."]
+# for x in liste:
+#     print(x)
+
+i = 0
+while i < len(liste):
+    print(liste[i])
+    i += 1
