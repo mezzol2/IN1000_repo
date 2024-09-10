30d462a4d6c81c1786494b1d28b35949d25944bb
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-10 11:54:07 +0200

    Create and complete enkel_rutenett.py

---
+++ trix_oppgaver/uke4/enkel_rutenett.py
@@ -0,0 +1,16 @@
+o_liste = []
+o = "o"
+
+for x in range(5):
+    o_liste.append(o)
+
+stjerneliste = []
+star = "*"
+for x in range(5):
+    stjerneliste.append(star)
+
+rutenett = [o_liste, stjerneliste]
+rutenett.append(o_liste)
+
+for e in range(3):
+    print(rutenett[e])
