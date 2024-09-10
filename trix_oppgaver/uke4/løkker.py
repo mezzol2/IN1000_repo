e3e75db28467dd1c562c58a3ce16bc0179a7e4f7
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-10 11:35:54 +0200

    Add section 04.04 to løkker.py

--- trix_oppgaver/uke4/løkker.py
+++ trix_oppgaver/uke4/løkker.py
@@ -15,18 +15,34 @@
 #     print(liste[i])
 #     i += 1
 
-li1 = [6, -4, 7, -2, 8, -3, 9, -11]
+# li1 = [6, -4, 7, -2, 8, -3, 9, -11]
 
-minst = li1[0]
-for num in li1:
-    if num < minst:
-        minst = num
+# minst = li1[0]
+# for num in li1:
+#     if num < minst:
+#         minst = num
 
-print(minst)
+# print(minst)
 
-maks = li1[0]
-for num in li1:
-    if num >= maks:
-        maks = num
+# maks = li1[0]
+# for num in li1:
+#     if num >= maks:
+#         maks = num
 
-print(maks)
+# print(maks)
+
+
+aviser = ["Aftenposten", "VG", "Morgenbladet", "Dagbladet", "Klassekampen"]
+
+for x in aviser:
+    print(x)
+
+partall = {10, 8, 6, 4, 2, 0}
+
+for x in partall:
+    print(x)
+
+kallenavn = {"Roger" : "Roggis", "Magnus" : "Kluten", "Stine" : "Lappen", "Ingeborg" : "Skruen"}
+for x in kallenavn:
+    print(x)
+    print(kallenavn[x])
