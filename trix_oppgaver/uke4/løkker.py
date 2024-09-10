5e39cb28803ca51f9f415a405e384f7dadae6828
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-10 11:27:00 +0200

    Add 04.04 section of trix oppgaver

--- trix_oppgaver/uke4/løkker.py
+++ trix_oppgaver/uke4/løkker.py
@@ -6,11 +6,27 @@
 #     print(i)
 #     i += 1
 
-liste = ["Sauer", "er", "myke", "dyr."]
+#liste = ["Sauer", "er", "myke", "dyr."]
 # for x in liste:
 #     print(x)
 
-i = 0
-while i < len(liste):
-    print(liste[i])
-    i += 1
+# i = 0
+# while i < len(liste):
+#     print(liste[i])
+#     i += 1
+
+li1 = [6, -4, 7, -2, 8, -3, 9, -11]
+
+minst = li1[0]
+for num in li1:
+    if num < minst:
+        minst = num
+
+print(minst)
+
+maks = li1[0]
+for num in li1:
+    if num >= maks:
+        maks = num
+
+print(maks)
