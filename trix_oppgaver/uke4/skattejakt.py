f12438d5ec0de5d61bb79095ed80b3ce4cc1cde2
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-12 10:50:50 +0200

    Add a single player version of skattejakt.py game

--- trix_oppgaver/uke4/skattejakt.py
+++ trix_oppgaver/uke4/skattejakt.py
@@ -17,36 +17,45 @@ def print_skattekart(skattekart):
     for rad in skattekart:
         print(" ".join(rad))
 
-print_skattekart(skattekart)
-
 treasure = []
-loop = True
+inp = input("Do you want to play a 1 or 2 player game?\n")
 
-while (len(treasure) != 2) or loop:
-    treasure = []
-    inp = input("Where do you want to hide the treasure?\nEnter two numbers from 0 to 4: ")
-    inp = inp.split()
+if inp == "1":
+    from random import randint
+    treasure.append(randint(0,4))
+    treasure.append(randint(0,4))
+    skattekart[treasure[0]][treasure[1]] = "X"
+
+elif inp == "2":
+    loop = True
+
+    while (len(treasure) != 2) or loop:
+        treasure = []
+        inp = input("Where do you want to hide the treasure?\nEnter two numbers from 0 to 4: ")
+        inp = inp.split()
 
 
-    for i in range(len(inp)):
-        treasure.append(int(inp[i]))
-        
-    if len(treasure) != 2:
-        print("TWO numbers are needed. Please try again.\n")
-    else:
-        for i in treasure:
-            if i in range(5):
-                loop = False
-            else:
-                print("At least one number is not between 0 and 4.  Please try again.\n")
-                break
+        for i in range(len(inp)):
+            treasure.append(int(inp[i]))
+            
+        if len(treasure) != 2:
+            print("TWO numbers are needed. Please try again.\n")
+        else:
+            for i in treasure:
+                if i in range(5):
+                    loop = False
+                else:
+                    print("At least one number is not between 0 and 4.  Please try again.\n")
+                    break
 
-print(treasure)
-skattekart[treasure[0]][treasure[1]] = "X"
-print_skattekart(skattekart)
+    print(treasure)
+    skattekart[treasure[0]][treasure[1]] = "X"
+    print_skattekart(skattekart)
 
-for i in range(50):
-    print()
+    for i in range(50):
+        print()
+else:
+    exit("That was an invalid response.  Try again.")
 
 print("You have 3 guesses to find the treasure.")
 for i in range(3):
