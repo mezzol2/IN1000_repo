44b2826dc62d6e35cbc82a43cf5979a5c68e39b2
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-12 10:37:58 +0200

    Complete skattejakt.py

---
+++ trix_oppgaver/uke4/skattejakt.py
@@ -0,0 +1,69 @@
+skattekart = [] # n lister med n elementer
+n = 5 # størrelsen
+
+# Lag n antall rader (lister)
+for i in range(n):
+    rad = []
+
+    # Fyll listene med bokstaven "O"
+    # n antall ruter per rad
+    for e in range(n):
+        rad.append("O")
+
+    # Legg til hver rade ("O"-liste) i skattekart-listen vår
+    skattekart.append(rad)
+
+def print_skattekart(skattekart):
+    for rad in skattekart:
+        print(" ".join(rad))
+
+print_skattekart(skattekart)
+
+treasure = []
+loop = True
+
+while (len(treasure) != 2) or loop:
+    treasure = []
+    inp = input("Where do you want to hide the treasure?\nEnter two numbers from 0 to 4: ")
+    inp = inp.split()
+
+
+    for i in range(len(inp)):
+        treasure.append(int(inp[i]))
+        
+    if len(treasure) != 2:
+        print("TWO numbers are needed. Please try again.\n")
+    else:
+        for i in treasure:
+            if i in range(5):
+                loop = False
+            else:
+                print("At least one number is not between 0 and 4.  Please try again.\n")
+                break
+
+print(treasure)
+skattekart[treasure[0]][treasure[1]] = "X"
+print_skattekart(skattekart)
+
+for i in range(50):
+    print()
+
+print("You have 3 guesses to find the treasure.")
+for i in range(3):
+    guess = []
+    inp = input(f"What is guess number {i+1}?\n").split()
+    
+    for g in range(len(inp)):
+        guess.append(int(inp[g]))
+    
+    if guess == treasure:
+        print(f"Congratulations! {guess} is correct!")
+        break
+    else:
+        print(f'{guess} is not correct. You have {3-i-1} guesses remaining.')
+        skattekart[guess[0]][guess[1]] = "#"
+
+if guess != treasure:
+    print("You did not find the treasure.  You are a shit pirate.")
+
+print_skattekart(skattekart)
