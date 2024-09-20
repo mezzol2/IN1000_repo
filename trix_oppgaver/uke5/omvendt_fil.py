30d797f3d372455ee6da14c849ca4ba5c4ec90e9
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-20 13:18:58 +0200

    Edit omvendt_fil.py so that it will write a linebreak after each line

--- trix_oppgaver/uke5/omvendt_fil.py
+++ trix_oppgaver/uke5/omvendt_fil.py
@@ -2,14 +2,14 @@ def omvendt_fil(input_filnavn, output_filnavn):
     file = open(input_filnavn, 'r')
     lines = []
     for line in file:
-        lines.append(line)
+        lines.append(line.strip())
 
     file.close()
     reversed_lines = lines[::-1]
 
     file = open(output_filnavn, 'w')
     for line in reversed_lines:
-        file.write(line)
+        file.write(f'{line}\n')
 
     
     file.close()
