c723b84df347892dc6c1af02173ed206741a27ab
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-24 17:59:48 +0200

    Create and complete roter_matrise.py for trix 06.09

---
+++ trix_oppgaver/uke6/roter_matrise.py
@@ -0,0 +1,25 @@
+def invert_matrix(matrix1):
+
+    matrix2 = []
+
+    for x in range(len(matrix1)):
+        row = []
+        for y in range(len(matrix1)):
+            row.append(matrix1[y][x])
+        matrix2.append(row)
+
+
+    return matrix2
+
+
+
+
+matriseEn = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+]
+
+matrix2 = invert_matrix(matriseEn)
+
+print(matrix2)
