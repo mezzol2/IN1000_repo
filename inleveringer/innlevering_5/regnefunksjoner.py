a69fc201a1cf7c61f0390be68960ec3d7675e510
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-09-25 16:49:21 +0200

    Write the final procedure in regnefunksjoner.py so that the program will run

--- inleveringer/innlevering_5/regnefunksjoner.py
+++ inleveringer/innlevering_5/regnefunksjoner.py
@@ -1,3 +1,6 @@
+#This program defines several calculation functions, and them calls them with
+# a final procedure at the end
+
 #Define an addition function
 def addisjon(a,b):
     a += b
@@ -31,6 +34,7 @@ assert divisjon(100,-20) == -5
 assert divisjon(-100, -20) == 5
 
 
+
 #Define a function that converts inches (tommer) to centimeters
 def tommer_til_cm(antall_tommer):
     #Check that antall_tommer is larger than 0
@@ -42,3 +46,34 @@ def tommer_til_cm(antall_tommer):
 
 #Test the tommer_til_cm function
 assert tommer_til_cm(1) == 2.54
+
+
+
+#Define a procedure that incoporates and calls the previous functions
+def skriv_beregninger():
+
+    #Prompt the user for two numbers
+    a = float(input("\nPlease write a number: "))
+    b = float(input("Please write another number: "))
+
+    #Use the given numbers to calcuate the addition, subtraction and division
+    add = addisjon(a,b)
+    sub = subtraksjon(a,b)
+    div = divisjon(a,b)
+
+    #Print the results to the terminal
+    print(f"\nThe sum of the two numbers is {add:.2f}.")
+    print(f"The difference of the two numbers is {sub:.2f}.")
+    print(f"The quotient of the two numbers is {div:.2f}.")
+
+    #Prompt the user for a value to convert
+    a = float(input("\nWrite a number to convert from inches to centimeters: "))
+    #Convert the value to centimeters
+    cm = tommer_til_cm(a)
+    #Print the result
+    print(f'{a:.2f} inches is {cm:.2f} centimeters.\n')
+
+
+
+#Call skriv_beregning to run the program
+skriv_beregninger()
