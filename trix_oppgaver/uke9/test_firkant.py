6098d5a2999e3426aa10a37e42c2715d2fc057ef
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-16 16:51:12 +0200

    Test the legg_til_side method

--- trix_oppgaver/uke9/test_firkant.py
+++ trix_oppgaver/uke9/test_firkant.py
@@ -9,6 +9,15 @@ def main():
     box = Firkant()
     assert not box.er_fullstendig()
 
+    bottom = Side(2, 'red')
+    left = Side(5, 'green')
+    right = Side(7, 'yellow')
 
+    box.legg_til_side(top, 't')
+    box.legg_til_side(bottom, 'b')
+    box.legg_til_side(left, 'l')
+    box.legg_til_side(right, 'r')
+
+    assert box.er_fullstendig()
 
 main()
