4b9cd4b933f7bb09be9392ace84a26d2b3faaa04
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-16 17:02:26 +0200

    Create and test sjekk_firkanter function, and finish trix 09.06

--- trix_oppgaver/uke9/test_firkant.py
+++ trix_oppgaver/uke9/test_firkant.py
@@ -1,6 +1,12 @@
 from side import Side
 from firkant import Firkant
 
+def sjekk_firkanter(shape_list):
+    for shape in shape_list:
+        if not shape.er_fullstendig():
+            return False
+    return True
+
 def main():
     top = Side(10, 'blue')
     assert top.get_color() == 'blue'
@@ -22,5 +28,20 @@ def main():
 
     box.fjern_side('l')
     assert not box.er_fullstendig()
+
+    #make one more box
+    box1 = Firkant()
+    box1.legg_til_side(top, 't')
+    box1.legg_til_side(bottom, 'b')
+    box1.legg_til_side(left, 'l')
+    box1.legg_til_side(right, 'r')
+
+    #Test the sjekk_firkanter method
+    box_list = [box1]
+    assert sjekk_firkanter(box_list)
+    
+    box_list.append(box)
+    assert not sjekk_firkanter(box_list)
+
 
 main()
