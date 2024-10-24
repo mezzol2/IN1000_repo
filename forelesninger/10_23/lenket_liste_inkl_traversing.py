4e202ec677bf115c812259c79e67b283a3bcbdc4
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-24 08:21:06 +0200

    Attempt to fix the node code

--- forelesninger/10_23/lenket_liste_inkl_traversing.py
+++ forelesninger/10_23/lenket_liste_inkl_traversing.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Stasjon:
     def __init__(self, navn):
         self._navn = navn
@@ -10,27 +12,39 @@ class Stasjon:
         self._naboer.append(nabo)
     
     def hent_neste(self):
-        return self._neste
+        return self._naboer[randint(0,len(self._naboer)-1)]
         
         
 def bygg_trase():
-    trikkestall = Stasjon("stallen")
-    forrige = trikkestall
+    stasjonsbok = {}
     for fil_linje in open("ruter.txt"):
         biter = fil_linje.strip()
         biter = biter.split()
         fra_stasjon_navn = biter[0]
         til_stasjon_navn = biter[1]
-        fra_stasjon = Stasjon(fra_stasjon_navn)
+
+
+        if not fra_stasjon_navn in stasjonsbok:
+            stasjonsbok[fra_stasjon_navn] = Stasjon(fra_stasjon_navn)
+        fra_stasjon = stasjonsbok[fra_stasjon_navn]
+    
+        if not til_stasjon_navn in stasjonsbok:
+            stasjonsbok[til_stasjon_navn] = Stasjon(til_stasjon_navn)
+        til_stasjon = stasjonsbok[til_stasjon_navn]
+            
+
         til_stasjon = Stasjon(til_stasjon_navn)
         fra_stasjon.legg_til_nabo(til_stasjon)
         til_stasjon.legg_til_nabo(fra_stasjon)
-    return trikkestall
+    return stasjonsbok
 
 
 def hoved_program():
-    denne = bygg_trase()
+    stasjonsbok = bygg_trase()
+    fra = input("Hvor reiser du fra?: ")
+    fra = stasjonsbok[fra]
     maal = input("Hvor vil du reise til?: ")
+    denne = fra
     while denne.hent_navn() != maal:
         print(f"Reiser gjennom {denne.hent_navn()}")
         denne = denne.hent_neste()
