f6065c10fee18a1ec44eba49a507417c87eb518c
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-29 12:00:40 +0100

    Create brev.py and test_brev.py for Trix 11.01

---
+++ trix_oppgaver/uke11/brev.py
@@ -0,0 +1,16 @@
+class Brev:
+    def __init__(self, avsender:str, mottaker:str):
+        self._avsender = avsender
+        self._mottaker = mottaker
+        self._innhold = []
+    
+    def skriv_linje(self, tekst_streng:str):
+        self._innhold.append(tekst_streng)
+    
+    def les_brev(self):
+        print(f"Hei, {self._mottaker}!\n")
+        
+        pass
+
+
+    
