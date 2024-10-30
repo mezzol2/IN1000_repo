418c184a976cfc75af1c57e909cb28f5185e38b4
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-30 12:40:07 +0100

    Create the 'skeleton files' for innlevering 8

---
+++ inleveringer/innlevering_8/celle.py
@@ -0,0 +1,25 @@
+class Celle:
+    # Konstruktør
+    def __init__(self):
+        pass
+    
+    def sett_doed(self):
+        pass
+
+    def sett_levende(self):
+        pass
+
+    def er_levende(self):
+        pass
+
+    def hent_status_tegn(self):
+        pass
+
+    def legg_til_nabo(self, nabo):
+        pass
+
+    def tell_levende_naboer(self):
+        pass
+
+    def oppdater_status(self):
+        pass
---
+++ inleveringer/innlevering_8/rutenett.py
@@ -0,0 +1,36 @@
+from random import randint
+from celle import Celle
+
+class Rutenett:
+    def __init__(self, rader, kolonner):
+        pass
+
+    def _lag_tomt_rutenett(self):
+        pass
+
+    def _lag_tom_rad(self):
+        pass
+
+    def fyll_med_tilfeldige_celler(self):
+        pass
+
+    def lag_celle(self, rad, kol):
+        pass
+
+    def hent_celle(self, rad, kol):
+        pass
+
+    def tegn_rutenett(self):
+        pass
+
+    def _sett_naboer(self, rad, kol):
+        pass
+
+    def koble_celler(self):
+        pass
+
+    def hent_alle_celler(self):
+        pass
+
+    def antall_levende(self):
+        pass
