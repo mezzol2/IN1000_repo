4ccb15dcaa38fe3ff762e3d01964b8dff714c003
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-30 13:40:42 +0100

    Define the Dag class

---
+++ trix_oppgaver/uke11/regn/dag.py
@@ -0,0 +1,10 @@
+class Dag:
+    def __init__(self, dato:str, nedbørtall:float):
+        self._dato = dato
+        self._nedbørtall = nedbørtall
+
+    def get_date(self):
+        return self._dato
+    
+    def get_nedbørtall(self):
+        return self._nedbørtall
