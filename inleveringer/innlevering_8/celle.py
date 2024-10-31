93fb2a30488cbbd01b2c13cfe50dcfd562b5e710
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-31 10:27:42 +0100

    Define and test the Celle class for innlevering 8

--- inleveringer/innlevering_8/celle.py
+++ inleveringer/innlevering_8/celle.py
@@ -1,25 +1,50 @@
 class Celle:
     # Konstruktør
     def __init__(self):
-        pass
+        self._status = "doed"
+        self._naboer = []
+        self._ant_levende_naboer = 0
     
     def sett_doed(self):
-        pass
+        self._status = "doed"
 
     def sett_levende(self):
-        pass
+        self._status = "levende"
 
+    #Check is the cell is living or dead
     def er_levende(self):
-        pass
+        if self._status == "levende":
+            return True
+        elif self._status == "doed":
+            return False
 
+    #If the cell is alive, return 'O', otherwise return '.'
     def hent_status_tegn(self):
-        pass
+        if self.er_levende():  #if True
+            return "O"
+        else:
+            return "."
 
     def legg_til_nabo(self, nabo):
-        pass
+        self._naboer.append(nabo)
 
     def tell_levende_naboer(self):
-        pass
+        #Reset the ant_levende_naboer back to 0
+        self._ant_levende_naboer = 0
+        
+        #Count the number of living neighbors
+        for nabo in self._naboer:
+            if nabo.er_levende():
+                self._ant_levende_naboer += 1
 
     def oppdater_status(self):
-        pass
+        #Check if is the cell is alive:
+        if self.er_levende():
+            #Cell dies if it has any number of neighbors that is not 2 or 3
+            if self._ant_levende_naboer not in [2,3]:
+                self.sett_doed()
+        
+        #If the cell is dead, it becomes alive if it has exactly 3 neighbors
+        else:
+            if self._ant_levende_naboer == 3:
+                self.sett_levende()
