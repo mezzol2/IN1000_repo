f5f1f681e5267b145160fbb6872498f2d14e9a96
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-10-31 13:35:42 +0100

    Define and test the Verden class for innlevering 8

--- inleveringer/innlevering_8/verden.py
+++ inleveringer/innlevering_8/verden.py
@@ -2,10 +2,28 @@ from rutenett import Rutenett
 
 class Verden:
     def __init__(self, rader, kolonner):
-        pass
+        self._rutenett = Rutenett(rader, kolonner)
+        self._rutenett.fyll_med_tilfeldige_celler()
+        self._rutenett.koble_celler()
+        self._generasjonsnummer = 0
 
     def tegn(self):
-        pass
+        self._rutenett.tegn_rutenett()
+        print(f"Generasjon: {self._generasjonsnummer}")
+        print(f"Levende celler: {self._rutenett.antall_levende()}")
+
 
     def oppdatering(self):
-        pass
+        #Make a list of all cells
+        celle_liste = self._rutenett.hent_alle_celler()
+
+        #Have each cell count how many living neighobrs it has
+        for celle in celle_liste:
+            celle.tell_levende_naboer()
+        
+        #Have each cell update its status
+        for celle in celle_liste:
+            celle.oppdater_status()
+
+        #Increase the generation number by 1
+        self._generasjonsnummer += 1
